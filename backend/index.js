f43e46c051d3f0e9be891d379d1f05410368d3ef
Prothito Shovon Majumder <turnoshovon1999@gmail.com>
2023-03-26 21:25:59 +0600

    Setting up connection to MongoDB cluster

--- backend/index.js
+++ backend/index.js
@@ -2,3 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+
+const app = express();
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://deversorium:iloveiplab@cluster0.flox5m0.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+                .then(() => app.listen(PORT, () => console.log('Server running on port: ' + PORT)))
+                .catch((error) => console.log(error.message));
