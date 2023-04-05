0cce0793514e54e0a7827e57fe74ce8e88abec12
Prothito Shovon Majumder <turnoshovon1999@gmail.com>
2023-04-06 00:41:14 +0600

    adding dotenv for hiding environment variables

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-
+import dotenv from 'dotenv';
 import hostelRoutes from './routes/hostels.js';
 import tenantRoutes from './routes/tenants.js';
 
 const app = express();
+dotenv.config();
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
@@ -14,9 +15,8 @@ app.use(cors());
 app.use('/hostels',hostelRoutes);
 app.use('/tenants',tenantRoutes);
 
-const CONNECTION_URL = "mongodb+srv://deversorium:iloveiplab@cluster0.flox5m0.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
                 .then(() => app.listen(PORT, () => console.log('Server running on port: ' + PORT)))
                 .catch((error) => console.log(error.message));
