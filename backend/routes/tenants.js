b9739aebf320ffaf17d002e61c70cc45842ab5a5
Prothito Shovon Majumder <turnoshovon1999@gmail.com>
2023-04-07 00:54:26 +0600

    adding auth middleware to CUD of hostel and tenant

--- backend/routes/tenants.js
+++ backend/routes/tenants.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { getTenants, createTenant, updateTenant, deleteTenant } from '../controllers/tenants.js';
+import auth from '../middleware/auth.js';
+
 const router = express.Router();
 
 router.get('/', getTenants);
-router.post('/',createTenant);
-router.patch('/:id',updateTenant);
-router.delete('/:id',deleteTenant);
+router.post('/', auth, createTenant);
+router.patch('/:id', auth, updateTenant);
+router.delete('/:id', auth, deleteTenant);
 export default router;
