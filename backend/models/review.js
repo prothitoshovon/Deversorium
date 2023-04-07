f86094f319da0f856d2c6e8369056fc36eae5ef0
Prothito Shovon Majumder <turnoshovon1999@gmail.com>
2023-04-07 13:10:48 +0600

    review (for hostels) schema added

---
+++ backend/models/review.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = mongoose.Schema({
+    user_id: {type: String, required: true},
+    hostel_id: {type: String, required: true},
+    comments: {type: String, required: true},
+    date_posted: 
+    {
+        type: Date,
+        default: new Date()
+    }
+})
+
+const reviewModel = mongoose.model('reviewModel',reviewSchema);
+
+export default reviewModel;
