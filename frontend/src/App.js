f52163c6dc4dd653947c009d4e7c29443a2e3b96
Al_RADI <alradi9923@gmail.com>
2023-04-09 00:40:49 +0600

    Added Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import {Route, Routes} from "react-router"
 import ResponsiveAppBar from './components/Navbar/Navbar'
+import Homepage from './components/Owner/Homepage/Homepage'
+import Profile from './components/Owner/Profile/Profile'
+import Hostel from './components/Owner/Hostel/Hostel'
+import Mealsheet from './components/Owner/Mealsheet/Mealsheet'
+
 
 function App() {
   return (
     <div>
-        <h1>homepage i guess</h1>
+        
+            <ResponsiveAppBar/>
+            <Routes>
+                <Route path="/" element={<Homepage/>}/>
+                <Route path="Profile" element={<Profile/>}/>
+                <Route path="Hostel" element={<Hostel/>}/>
+                <Route path="Mealsheet" element={<Mealsheet/>}/>
+            </Routes>
+        
+        
     </div>
   )
 }
