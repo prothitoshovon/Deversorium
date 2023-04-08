12a49ea80b86cffc1005e8fe0b3b907782a1e742
Prothito Shovon Majumder <turnoshovon1999@gmail.com>
2023-04-08 21:31:55 +0600

    adding controller for reviews

---
+++ backend/controllers/reviews.js
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose';
+import reviewModel from '../models/review.js'
+
+export const getReviews = async(req, res) => {
+    try{
+        const reviews = await reviewModel.find();
+        console.log(reviews);
+        res.status(200).json(reviews);
+    } catch(error){
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const getReviewsByHostel = async(req,res) => {
+    const {id: _id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(_id))
+    {
+        return res.status(404).send('No hostels with that ID');
+    }
+    try{     
+        const reviews = await reviewModel.find({hostel_id: _id});
+        console.log(reviews);
+        res.status(200).json(reviews);
+    } catch(error){
+        res.status(404).json({message: error.message});
+    }
+} 
+
+export const getReviewsByUser = async(req,res) => {
+    const {id: _id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(_id))
+    {
+        return res.status(404).send('No tenants with that ID');
+    }
+    try{     
+        const reviews = await reviewModel.find({user_id: _id});
+        console.log(reviews);
+        res.status(200).json(reviews);
+    } catch(error){
+        res.status(404).json({message: error.message});
+    }
+}
+
+
+export const updateReview = async (req,res)=>{
+    const { id: _id} = req.params;
+    const review = req.body;
+    if(!mongoose.Types.ObjectId.isValid(_id))
+    {
+        return res.status(404).send('No review with that ID');
+    }
+
+    const updatedReview = await reviewModel.findByIdAndUpdate(_id, {...review, _id}, {new: true});
+    res.json(updatedReview);
+}
+
+export const deleteTenant = async (req,res)=>{
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(_id))
+    {
+        return res.status(404).send('No review with that ID');
+    }
+
+    await reviewModel.findByIdAndRemove(id);
+
+    res.json('Review Deleted Successfully');
+}
