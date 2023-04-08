220b63e62b4bf0b8077f19040c1899651b506bac
Al_RADI <alrad9923@gmail.com>
2023-04-08 23:35:16 +0600

    Just added some folder structures

---
+++ frontend/src/components/Owner/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div>
+      <h1>HomePage for owner</h1>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ frontend/src/components/Owner/Hostel/Hostel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hostel() {
+  return (
+    <div>
+      <h1>My hostel page for owner</h1>
+    </div>
+  )
+}
+
+export default Hostel
