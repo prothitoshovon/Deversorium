2a35fcb27dc63f85ab1aebd05e54ce29a9bce978
Prothito Shovon Majumder <turnoshovon1999@gmail.com>
2023-04-11 11:22:23 +0600

    missing phone number error fixed

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,14 +4,14 @@ import dotenv from 'dotenv';
 import UserModel from '../models/user.js';
 
 export const signin = async (req, res) => {
-    const {_email, _password, _role} = req.body;
+    const {email, password, role} = req.body;
 
     try{
-        const existingUser = await User.findOne({email: _email, role: _role});
+        const existingUser = await UserModel.findOne({email: email, role: role});
 
         if(!existingUser) return res.status(404).json({message: "User doesn't exist."});
 
-        const isPasswordCorrect = await bcrypt.compare(_password, existingUser.password);
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
         
         if(!isPasswordCorrect) return res.status(400).json({message: "Invalid credentials."});
 
@@ -24,7 +24,7 @@ export const signin = async (req, res) => {
 }
 
 export const signup = async (req, res) => {
-    const {email, password, confirmPassword, firstName, lastName, role} = req.body;
+    const {email, password, confirmPassword, firstName, lastName, role, phone} = req.body;
 
     try{
         const existingUser = await UserModel.findOne({email: email, role: role});
@@ -35,7 +35,7 @@ export const signup = async (req, res) => {
         
         const hashedPassword = await bcrypt.hash(password, 12);
         console.log('works 1')
-        const result = await UserModel.create({email: email, password: hashedPassword, name: `${firstName} ${lastName}`, role: role});
+        const result = await UserModel.create({email: email, password: hashedPassword, name: `${firstName} ${lastName}`, role: role, phone: phone});
         console.log('works 2')
        
         const token = jwt.sign({email: result.email, id: result._id, role: result.role}, process.env.TEST_TOKEN, {expiresIn: '1h'});
