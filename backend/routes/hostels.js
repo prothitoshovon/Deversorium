899c643d36043422504637a39d4fa7c6d44c1291
Prothito Shovon Majumder <turnoshovon1999@gmail.com>
2023-04-15 00:55:39 +0600

    added route for get hostel by owner ID

--- backend/routes/hostels.js
+++ backend/routes/hostels.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getHostels, createHostel, updateHostel, deleteHostel } from '../controllers/hostels.js';
+import { getHostels, createHostel, updateHostel, deleteHostel, getHostelByOwnerId } from '../controllers/hostels.js';
 import auth from '../middleware/auth.js';
 const router = express.Router();
 
 router.get('/', getHostels);
+router.get('/o/:id',getHostelByOwnerId);
 router.post('/', auth, createHostel);
 router.patch('/:id', auth, updateHostel);
 router.delete('/:id', auth, deleteHostel);
