c11f075812906d19bcb3532ebe1bd3cd0fd5ef0a
Al_RADI <alrad9923@gmail.com>
2023-04-16 02:04:28 +0600

    Add room works

---
+++ frontend/src/reducers/rooms.js
@@ -0,0 +1,18 @@
+import { FETCH_ALL, CREATE } from '../constants/actionTypes'
+
+export default (state = { isLoading: true, rooms: [] }, action) => {
+    switch(action.type)
+    {
+        case 'START_LOADING':
+            return { ...state, isLoading: true }
+        case 'END_LOADING':
+            return { ...state, isLoading: false }
+        case FETCH_ALL:
+            return action.payload
+        case CREATE:
+            return { ...state, rooms: [...state.rooms, action.payload] };
+
+        default:
+            return state
+    }
+}
