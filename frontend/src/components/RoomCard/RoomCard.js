dea4c28c1b6b6df7437e6f0cc27250ddfbe4c6a7
Al_RADI <alrad9923@gmail.com>
2023-04-18 22:55:11 +0600

    Room card shows real data

--- frontend/src/components/RoomCard/RoomCard.js
+++ frontend/src/components/RoomCard/RoomCard.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import { Box,Card, CardActions, CardContent, CardMedia, Typography, ButtonBase } from '@material-ui/core/';
 import { Button } from '@mui/material';
-function RoomCard() {
+import { useDispatch, } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+function RoomCard({ room, setCurrentId }) {
+
+    const user = JSON.parse(localStorage.getItem('profile'));
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userId = user?.result?._id;
+    const book = ()=>{
+        //When book is called 
+        //We first validate if user has already booked before 
+        //If yes , then prompt user that they can only book once
+        //iF no , ask user to confirm if he wants to go on
+    }
   return (
     <Box width='600px' style={{marginTop:"20px", marginLeft:"10px"}}>
         <Card raised elevation={6}>
@@ -14,10 +27,13 @@ function RoomCard() {
             </CardMedia>
             <CardContent>
                 <Typography gutterBottom variant='h5' component='div'>
-                Room card lets go
+                Will need to query for Hostel name and address
                 </Typography>
                 <Typography variant='body2' >
-                Wee woo wee woo
+                {room.rent} BDT
+                </Typography>
+                <Typography variant='body2' >
+                {room.area} sqft
                 </Typography>
             </CardContent>
             <CardActions>
