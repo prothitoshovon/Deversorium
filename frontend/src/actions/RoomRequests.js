5c95e358794011c2a41c4d13362ab3e9558616e6
Al_RADI <alradi9923@gmail.com>
2023-04-19 22:19:15 +0600

    Room requests coming through

--- frontend/src/actions/RoomRequests.js
+++ frontend/src/actions/RoomRequests.js
@@ -17,6 +17,7 @@ export const getRoomRequestsByHostelId = (id) => async (dispatch) =>{
   try {
     dispatch({ type: START_LOADING });
     const { data } = await api.getRoomRequestsByHostelId(id);
+    
     dispatch({ type: FETCH_ROOM_REQUEST_BY_HOSTEL_ID, payload: { roomRequests: data } });
     dispatch({type: END_LOADING})
   } catch (error) {
