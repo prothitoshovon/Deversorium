e4cef95eca5b1d7124520da4ccd76d16a379c813
Prothito Shovon Majumder <turnoshovon1999@gmail.com>
2023-04-19 21:01:02 +0600

    updated owner schema, createHostel to contain h_id

--- backend/models/owner.js
+++ backend/models/owner.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const ownerSchema = mongoose.Schema({
     user_id: String,
-    hostel_id:[String],
-    hostel_name: [String]
+    hostel_id:String,
+    hostel_name: String
 })
 
 const ownerModel = mongoose.model('ownerModel',ownerSchema);
