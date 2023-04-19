d9f4e551575d9fe064b89b557ac195a7b1de09cb
Al_RADI <alrad9923@gmail.com>
2023-04-20 00:24:27 +0600

    Styled room request card

--- frontend/src/components/RoomRequestCard/RoomRequestCard.js
+++ frontend/src/components/RoomRequestCard/RoomRequestCard.js
@@ -4,12 +4,14 @@ import { Button } from '@mui/material';
 import { useDispatch, } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import cardImage from '../../images/Order Placed.png'
+import useStyles from './styles.js'
 function RoomRequestCard({ roomRequest, setCurrentId }) {
 
     const user = JSON.parse(localStorage.getItem('profile'));
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const userId = user?.result?._id;
+    const classes = useStyles();
     const allow = ()=>{
 
         //When allow is called,
@@ -21,38 +23,38 @@ function RoomRequestCard({ roomRequest, setCurrentId }) {
         // hostel_id: {type: String, required: true},
         // hostel_name: String,
         // date_issued: Date
-
+        //Find the tenant
+        //Find the room 
+        //
         // roomRequest.user_id is to be queried to find the tenant 
         // then that user needs to be updated so their room_id is roomRequest.room_id 
 
     }
     
   return (
-    <Box width='600px' style={{marginTop:"20px", marginLeft:"10px"}}>
-        <Card raised elevation={6}>
+    // <Box width='600px' style={{marginTop:"20px", marginLeft:"10px"}}>
+        <Card className={classes.card}raised elevation={6}>
             <CardMedia
-                component="img"
-                height='200'
+                className={classes.media}
                 image={cardImage}
             >
-
             </CardMedia>
-            <CardContent>
+            <CardContent className={classes.overlay}>
                 <Typography gutterBottom variant='h5' component='div'>
-                This is where the card title will be
+                user's name
                 </Typography>
                 <Typography variant='body2' >
-                Weee wooo wee wooo
+                Room number
                 </Typography>
                 <Typography variant='body2' >
-                wooo weee wooo weee
+                user's phone number
                 </Typography>
             </CardContent>
-            <CardActions>
+            <CardActions className={classes.cardActions}>
                 <Button size='small' onClick={allow}>Allow tenant</Button>
             </CardActions>
         </Card>
-    </Box>
+    // </Box>
     
   )
 }
