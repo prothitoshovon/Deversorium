121236a232023da74fa9275ed56511a0544c6e5e
Prothito Shovon Majumder <turnoshovon1999@gmail.com>
2023-04-28 11:34:35 +0600

    updated schema

--- backend/models/review.js
+++ backend/models/review.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const reviewSchema = mongoose.Schema({
     user_id: {type: String, required: true},
+    user_name: String,
     hostel_id: {type: String, required: true},
     comments: {type: String, required: true},
     date_posted: 
