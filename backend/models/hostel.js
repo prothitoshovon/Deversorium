42aa3b64d49c232370d85ea5b145b20c10f9a836
Prothito Shovon Majumder <turnoshovon1999@gmail.com>
2023-05-01 20:34:46 +0600

    generate and receive bill functions and routes

--- backend/models/hostel.js
+++ backend/models/hostel.js
@@ -9,7 +9,7 @@ const hostelSchema = mongoose.Schema({
     },
     owner_id: String,
     owner_name: String,
-    last_bill_generated: Date
+    last_bill_generated_date: Date
 })
 
 const hostelModel = mongoose.model('hostelModel',hostelSchema);
