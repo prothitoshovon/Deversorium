9eafd06400ec1eaf005dfa9ec4c233e297f01faf
Prothito Shovon Majumder <turnoshovon1999@gmail.com>
2023-05-01 20:02:33 +0600

    added get tenant by hostel id

--- backend/routes/tenants.js
+++ backend/routes/tenants.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getTenants, createTenant, updateTenant, deleteTenant, getTenantsByUserId } from '../controllers/tenants.js';
+import { getTenants, createTenant, updateTenant, deleteTenant, getTenantsByUserId, getTenantsByHostelId } from '../controllers/tenants.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', getTenants);
+router.get('/h/:id',getTenantsByHostelId);
 router.get('/:id',getTenantsByUserId);
 router.post('/', auth, createTenant);
 router.patch('/:id', auth, updateTenant);
