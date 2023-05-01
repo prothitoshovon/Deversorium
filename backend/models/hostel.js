c1eb868bc3aaf430d7a34112908f7a82d3a8069a
Prothito Shovon Majumder <turnoshovon1999@gmail.com>
2023-05-01 19:58:24 +0600

    updating hostel and tenant schema

--- backend/models/hostel.js
+++ backend/models/hostel.js
@@ -8,7 +8,8 @@ const hostelSchema = mongoose.Schema({
         default: 'Not Available'
     },
     owner_id: String,
-    owner_name: String
+    owner_name: String,
+    last_bill_generated: Date
 })
 
 const hostelModel = mongoose.model('hostelModel',hostelSchema);
