25e19e8abee0719ca6014c29b6608b04ad2128ca
Prothito Shovon Majumder <turnoshovon1999@gmail.com>
2023-05-01 20:34:46 +0600

    generate and receive bill functions and routes

--- backend/routes/hostels.js
+++ backend/routes/hostels.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { getHostels, createHostel, updateHostel, deleteHostel, getHostelByOwnerId, getHostelByHostelId } from '../controllers/hostels.js';
+import { getHostels, createHostel, updateHostel, deleteHostel, getHostelByOwnerId, getHostelByHostelId, generateBill, receiveBill } from '../controllers/hostels.js';
 import auth from '../middleware/auth.js';
 const router = express.Router();
 
 router.get('/', getHostels);
 router.get('/o/:id',getHostelByOwnerId);
 router.get('/h/:id',getHostelByHostelId);
+router.patch('/bills/create/:id',generateBill);
+router.patch('/bills/receive/:uid',receiveBill);
 router.post('/', auth, createHostel);
 router.patch('/:id', auth, updateHostel);
 router.delete('/:id', auth, deleteHostel);
