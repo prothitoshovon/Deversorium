b54ec6e94cddf3651e2462b0210e678b4acac768
Al_RADI <alradi9923@gmail.com>
2023-05-06 20:51:34 +0600

    Profile update done

--- frontend/src/reducers/auth.js
+++ frontend/src/reducers/auth.js
@@ -4,7 +4,7 @@ const authReducer = (state = { authData: null }, action) => {
   switch (action.type) {
     case actionType.AUTH:
       localStorage.setItem('profile', JSON.stringify({ ...action?.data }));
-
+      console.log({...action?.data})
       return { ...state, authData: action.data, loading: false, errors: null };
     case actionType.LOGOUT:
       localStorage.clear();
